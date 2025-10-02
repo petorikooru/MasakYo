e6d1b543095819302f112d7670d778751aaa1843
Daffa Alwana <daffcloud@gmail.com>
2025-10-02 09:00:31 +0700

    Adding the admin UI
    pls help me. just finish the admin ui (2/3), remaining admin users

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@data': path.resolve(__dirname, './src/data'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-    }
-  }
+  plugins: [react(), tailwindcss()],
 })
